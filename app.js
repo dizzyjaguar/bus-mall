613aabf68a95490441196f1ee52c6998ebf569a9
Eli Zevin <dizzyjaguar@gmail.com>
2020-01-28 10:50:41 -0800

    create basic getElements in app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// imports 
+
+const h2Item1 = document.getElementById('item-1-h2');
+const h2Item2 = document.getElementById('item-2-h2');
+const h2Item3 = document.getElementById('item-3-h2');
+const imgItem1 = document.getElementById('item-1-img');
+const imgItem2 = document.getElementById('item-2-img');
+const imgItem3 = document.getElementById('item-3-img');
+
+const productRadioTags = document.querySelectorAll('input');
+
+const submitButton = document.getElementById('submitButton');
