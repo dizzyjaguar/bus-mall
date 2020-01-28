cf8c748b2b8702da6614d0841ae0172702402ce7
Owen Prendergast <prendergasto@gmail.com>
2020-01-28 10:54:35 -0800

    fixed all the data

---
+++ random-image.js
@@ -0,0 +1,10 @@
+import { busMallData } from './data.js';
+
+export function getRandomImage() {
+    const randomImages = document.getElementById[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    const randomNumber = Math.floor(Math.random() * 20);
+    // here we are multiplying the computer's choices by the const "randomNumber" in order to siumulate a choice between rock paper n scissors.
+    return randomImages[randomNumber];
+    console.log('randomImages' + randomImages);
+}
+
